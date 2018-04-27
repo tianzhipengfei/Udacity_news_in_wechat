4bb4a2fde7bf2d107d07ad81fb640d7667d81c62
Eric Lee <869909541@qq.com>
2018-04-27 13:27:14 +0800

    新增滑动新闻页切换新闻的功能，以及自定义导航栏

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,25 +1,28 @@
 // pages/detail/detail.js
+var newID = 0;
+var touchDirection = 0;
+var touchDot = 0;//触摸时的原点
+var time = 0;// 时间记录，用于滑动时且时间小于1s则执行左右滑动
+var interval = "";// 记录/清理时间记录
+
+
 Page({
   data: {
-      contentList: [
-        {
-          "type": "image",
-          "src": "/images/404-bg.jpg",
-          "id": 2
-        },
-        {
-            "type": "strong",
-            "text": "&nbsp;你来到了没有知识的荒原 :(",
-            "id": 1
-        }
-      ],
-    countNum: 0,
-    title: "404 Not Found",
-    date: "Futrue",
+      contentList: [],
+    countNum: "",
+    title: "",
+    date: "",
+    generateOk:false,
   },
   onLoad(options){
-      let newsID = options ? options.newsID : 0;
-      this.getNews(newsID);
+      newID = options ? options.newsID : 0;
+      this.getNews(newID);
+      setTimeout(() => {
+          this.setData({
+              generateOk: true
+          })
+      }, 800)
+
   },
   getNews(newsID){
       wx.request({
@@ -36,9 +39,28 @@ Page({
             this.setData({
                 title: result.title,
                 date: result.date,
-                countNum: result.readCount,
+                countNum: "阅读 "+result.readCount,
                 contentList: result.content
             })
+          },
+          fail:()=>{
+              this.setData({
+                  contentList: [
+                      {
+                          "type": "image",
+                          "src": "/images/404-bg.jpg",
+                          "id": 2
+                      },
+                      {
+                          "type": "strong",
+                          "text": "&nbsp;你来到了没有知识的荒原 :(",
+                          "id": 1
+                      }
+                  ],
+                  countNum: "阅读 " + 0,
+                  title: "404 Not Found",
+                  date: "Futrue"
+              })
           }
       })
   },
@@ -47,5 +69,57 @@ Page({
           result.source = "网络媒体"
       }
       result.date = result.source + "   " + result.date.slice(11, 16);
-  } 
+  },
+  backToHome(){
+      wx.navigateBack({
+          delta: 1
+      })
+  },
+  // 触摸开始事件
+  touchStart: function (e) {
+      touchDot = e.touches[0].pageX; // 获取触摸时的原点
+      // 使用js计时器记录时间  
+      interval = setInterval(function () {
+          time++;
+      }, 100);
+  },
+  // 触摸移动事件
+  touchMove: function (e) {
+      var touchMove = e.touches[0].pageX;
+      // 向左滑动  
+      if (touchMove - touchDot <= -180 && time < 100) {
+          touchDirection = 1;
+      }
+      // 向右滑动
+      if (touchMove - touchDot >= 180 && time < 100) {
+          touchDirection = -1;
+      }
+  },
+  // 触摸结束事件
+  touchEnd: function (e) {
+      if (touchDirection == -1) {
+          console.log("right");
+      } else if (touchDirection == 1) {
+        console.log("left");
+      } 
+      if (touchDirection != 0)
+      this.changeNews(touchDirection);
+      clearInterval(interval); // 清除setInterval
+      time = 0;
+      touchDirection = 0;
+  },
+  changeNews(num){
+      this.setData({
+          generateOk: false
+      });
+      console.log(newID);
+      newID = parseInt(newID) + num;
+      console.log(newID);
+      this.getNews(newID);
+      setTimeout(() => {
+          this.setData({
+              generateOk: true
+          })
+      }, 800);
+  }
 })
